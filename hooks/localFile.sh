adb10f9dfe464009440bf0fe3f435bddb52552da
v-weiyding <v-weiyding@microsoft.com>
2024-08-01 16:19:51 +0800

    Linux dev

--- hooks/localFile.sh
+++ hooks/localFile.sh
@@ -1,3 +1,3 @@
 echo  "\n AZD HOOK LOG : $1 ================="
-echo  " AZD HOOK LOG : AZURE_ENV_NAME $AZURE_ENV_NAME \n"
+echo  " AZD HOOK LOG : AZURE_ENV_NAME $AZURE_ENV_NAME "
 echo  " AZD HOOK LOG : test $0 \n"
